701e93399b3c2cc5475c5fbdbc7ab1ebbbcc017e
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-09-21 18:56:14 +0700

    set dependency.

---
+++ packages/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+
+import Nav from 'react-bootstrap/Nav'
+import NavBar from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/src/component/edit-post.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditPost extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Edit Forum Component!</p>
+      </div>
+    );
+  }
+}
